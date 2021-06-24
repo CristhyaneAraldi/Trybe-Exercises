7724dd01d89e2395d8fbb31bc563b7360949c556
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-06-24 18:27:41 -0300

    Conclui os exercicios bloco 4 dia 2

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -61,3 +61,42 @@ if (result === 0) {
 } else {
   console.log(result);
 }
+
+//exercicio 7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável smallestNumber poderia ser qualquer um, desde que fosse maior que o maior número do array numbers. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+let smallestNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+  }
+}
+
+console.log(smallestNumber);
+
+//exercicio 8
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+console.log(numbers);
+
+//exercicio 9
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+console.log(numbers);
+
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index] / 2);
+};
