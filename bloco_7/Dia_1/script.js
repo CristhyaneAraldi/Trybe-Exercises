13e8fb2f3a1b7403f091b6dd1ff0ea43a860f9c7
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-07-25 13:58:35 -0300

    Continuaçao exercicios dia 7.1

--- bloco_7/Dia_1/script.js
+++ bloco_7/Dia_1/script.js
@@ -27,3 +27,58 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+// Exercício 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function ordenaArray() {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[4] = 7;
+  oddsAndEvens[5] = 10;
+  oddsAndEvens[6] = 13;
+
+  return oddsAndEvens
+}
+
+console.log(ordenaArray(oddsAndEvens));
+
+// outra forma
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
+// usando o método array.sort()
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort();
+
+console.log(oddsAndEvens); 
+// [ 10, 13, 2, 3, 4, 7 ] devido a  pontuação de código unicode.
+
+// retornar ordenada
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function compararNumeros(a, b) {
+  return a - b;
+}
+
+console.log(oddsAndEvens.sort(compararNumeros));
+
+// simplificando
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(oddsAndEvens);
