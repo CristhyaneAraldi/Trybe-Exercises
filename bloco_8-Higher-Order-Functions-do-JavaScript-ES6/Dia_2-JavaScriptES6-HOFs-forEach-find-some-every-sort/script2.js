bf5073623dc23cfcc3ac4c44a432969b5452fb01
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-07-29 16:27:09 -0300

    Bloco 8 dia 1 conteudos - exercicios para fixar 2

--- bloco_8-Higher-Order-Functions-do-JavaScript-ES6/Dia_2-JavaScriptES6-HOFs-forEach-find-some-every-sort/script2.js
+++ bloco_8-Higher-Order-Functions-do-JavaScript-ES6/Dia_2-JavaScriptES6-HOFs-forEach-find-some-every-sort/script2.js
@@ -1,4 +1,4 @@
-// Conteudos, arrow.find
+// Conteudos, array.find
 
 // Encontrar e imprimir no console o primeiro elemento de um array que satisfaça a uma determinada condição
 // Usando for e if
@@ -24,3 +24,20 @@ const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
 
 console.log(firstMultipleOf5);
 // 50
+
+// exemplo 2
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30
---
+++ bloco_8-Higher-Order-Functions-do-JavaScript-ES6/exercicio3.js
@@ -0,0 +1,4 @@
+// Para fixar
+// Array.find
+ 
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
