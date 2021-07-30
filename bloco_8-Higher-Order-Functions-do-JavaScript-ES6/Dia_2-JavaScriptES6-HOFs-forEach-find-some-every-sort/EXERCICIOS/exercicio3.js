e6148086d25193ec7367d85c771487a6abd5e82f
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-07-30 10:28:38 -0300

    Bloco 8 dia 2 exercicio 3

--- bloco_8-Higher-Order-Functions-do-JavaScript-ES6/Dia_2-JavaScriptES6-HOFs-forEach-find-some-every-sort/EXERCICIOS/exercicio3.js
+++ bloco_8-Higher-Order-Functions-do-JavaScript-ES6/Dia_2-JavaScriptES6-HOFs-forEach-find-some-every-sort/EXERCICIOS/exercicio3.js
@@ -1,6 +1,6 @@
 // BLOCO 8 DIA 2
 // EXERCICIOS
-// Array.forEach
+// Array.find
 
 const assert = require('assert');
 
@@ -68,3 +68,19 @@ const books = [
 ];
 
 // 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
+};
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
