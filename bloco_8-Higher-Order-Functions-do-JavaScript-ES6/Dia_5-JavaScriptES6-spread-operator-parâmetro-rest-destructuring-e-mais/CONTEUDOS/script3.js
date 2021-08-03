49a55707b616226d7acccbfe8dc35d8e0c64930d
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-08-03 20:22:39 -0300

    Bloco 8 Dia 5 - conteudos 3

---
+++ bloco_8-Higher-Order-Functions-do-JavaScript-ES6/Dia_5-JavaScriptES6-spread-operator-parâmetro-rest-destructuring-e-mais/CONTEUDOS/script3.js
@@ -0,0 +1,19 @@
+// CONTEUDOS
+//Default Destructuring 
+
+// Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
+const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
