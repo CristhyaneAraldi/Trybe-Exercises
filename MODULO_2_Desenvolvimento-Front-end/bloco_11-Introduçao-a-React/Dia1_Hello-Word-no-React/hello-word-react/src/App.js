6384e9a585acbaee3cdaa153446e49b1e6d0f6d5
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-08-30 18:29:23 -0300

    Bloco 11 Dia 1 - Introduçao ao React

---
+++ MODULO_2_Desenvolvimento-Front-end/bloco_11-Introduçao-a-React/Dia1_Hello-Word-no-React/hello-word-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; // usado sempre que tiver jsx no arquivo
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    // <> 
+    //   <span className="hello-world">Hello, world!</span>
+    //   <p>Esse é o meu primeiro App React</p>
+    // </>
+    <HelloWorld/>
+  );
+}
+
+export default App;
---
+++ MODULO_2_Desenvolvimento-Front-end/bloco_11-Introduçao-a-React/Dia1_Hello-Word-no-React/hello-word-react/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends React.Component { // classe JS com código react imbutido
+  render() {
+    return <h1 className="hello-world">Hello, World!</h1>
+  }
+}
+
+export default HelloWorld
