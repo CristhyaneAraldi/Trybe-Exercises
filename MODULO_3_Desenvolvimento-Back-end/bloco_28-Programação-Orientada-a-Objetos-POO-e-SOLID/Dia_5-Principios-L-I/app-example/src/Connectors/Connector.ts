21ead01717573400e17f589ae11474fcd3bc9bd9
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2022-03-09 19:50:37 -0300

    Bloco 28 Dia 5

--- MODULO_3_Desenvolvimento-Back-end/bloco_28-Programação-Orientada-a-Objetos-POO-e-SOLID/Dia_5-Principios-L-I/app-example/src/Connectors/Connector.ts
+++ MODULO_3_Desenvolvimento-Back-end/bloco_28-Programação-Orientada-a-Objetos-POO-e-SOLID/Dia_5-Principios-L-I/app-example/src/Connectors/Connector.ts
@@ -1,11 +1,37 @@
 // .src/Connectors/Connector.ts
 
-export interface Connector {
+// export interface Connector {
+//   getCount(token: string): number | Promise<number>;
+
+//   incrementCount(token: string): void;
+  
+//   closeConnection(): void;
+
+//   clearCount(token: string): void;
+
+//   firstCount(token: string): void;
+// }
+
+// export interface ConnectorConstructor {
+//   host: string;
+//   port: number;
+//   database?: string;
+//   user?: string;
+//   password?: string;
+// }
+
+// export default Connector;
+
+// ./src/Connectors/Connector.ts
+
+export interface ReadOnlyConnector {
   getCount(token: string): number | Promise<number>;
 
-  incrementCount(token: string): void;
-  
   closeConnection(): void;
+}
+
+export interface Connector extends ReadOnlyConnector {
+  incrementCount(token: string): void;
 
   clearCount(token: string): void;
 
